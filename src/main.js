eebb04f885f4e0c803220ccdad74e16fde8e39ea
gdd <1356871952@qq.com>
2020-07-16 23:51:23 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import Vuex from 'vuex'
+import store from './store'
+import router from './router'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+
+
+// Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
